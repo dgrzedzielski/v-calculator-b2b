620f97c2103c8554271252bf0274f3729b8e2a87
Dominik Grzędzielski <gie.dominik@gmail.com>
2020-03-12 19:24:22 +0100

    Add //TODO comment to remove some code after connecting with backend

--- src/modules/calculator/components/calculator-expense-form.vue
+++ src/modules/calculator/components/calculator-expense-form.vue
@@ -87,7 +87,7 @@
             }
         }
 
-        uuidv4() {
+        uuidv4() { // TODO REMOVE IT AFTER CONNECTING WITH SOME BACKEND
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 const r = Math.random() * 16 | 0;
                 const v = c === 'x' ? r : (r & 0x3 | 0x8);
@@ -101,7 +101,7 @@
             if (this.expenseToEdit) {
                 this.$emit('edit-expense', this.expense);
             } else {
-                this.expense.id = this.uuidv4();
+                this.expense.id = this.uuidv4(); // TODO REMOVE IT LIKE ABOVE
                 this.$emit('add-expense', this.expense);
             }
         }
