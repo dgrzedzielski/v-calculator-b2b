faa5acd0c07dc8c7d002e5e54a3de62f9e412b7a
Dominik Grzędzielski <gie.dominik@gmail.com>
2020-04-19 15:11:30 +0200

    Nav: Improve styles, separate template to base-nav

---
+++ src/core/components/layout/nav/app-nav-mobile/app-nav-mobile.vue
@@ -0,0 +1,22 @@
+<template>
+    <base-nav
+        v-bind="$props"
+        name="app-nav-mobile"
+    />
+</template>
+
+<script lang="ts">
+    import { defineComponent } from '@vue/composition-api';
+    import { ToPropType } from '@/core/types/nav';
+    import BaseNav from '../base-nav';
+    import NavMixin, { NavProps } from '../nav-mixin';
+
+    const AppNavMobile = defineComponent<NavProps>({
+        components: { BaseNav },
+        mixins: [NavMixin]
+    });
+
+    export default AppNavMobile;
+</script>
+
+<style lang="scss" src="./app-nav-mobile.scss" />
